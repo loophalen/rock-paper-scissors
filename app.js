f2756bb490caf898c7ff171b85390da35d44d2ae
loophalen <abharris@outlook.com>
2021-12-05 12:26:59 -0500

    computer generated choice

---
+++ app.js
@@ -0,0 +1,18 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+  })
+);
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1; // or you can use possibleChoices.length
+  console.log(randomNumber);
+}
